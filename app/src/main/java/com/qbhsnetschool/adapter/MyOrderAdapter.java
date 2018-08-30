3cbfd4f908b90780173def92688d4b3c16bdcd3b
liang <810901579@qq.com>
2018-08-30 21:00:53 +0800

    完成支付页面

--- app/src/main/java/com/qbhsnetschool/adapter/MyOrderAdapter.java
+++ app/src/main/java/com/qbhsnetschool/adapter/MyOrderAdapter.java
@@ -6,10 +6,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.qbhsnetschool.R;
 import com.qbhsnetschool.entity.CouponBean;
 import com.qbhsnetschool.entity.OrderBean;
+import com.qbhsnetschool.uitls.ConstantUtil;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -29,12 +34,44 @@ public class MyOrderAdapter extends RecyclerView.Adapter<MyOrderAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.order_item, viewGroup, false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_order_list_item, viewGroup, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        try {
+            OrderBean orderBean = orderBeans.get(position);
+            viewHolder.order_id.setText("订单号：" + orderBean.getOrder_no());
+            if (orderBean.getStatus() == 0){
+                viewHolder.pay_status.setText("未支付");
+                viewHolder.order_cancel.setVisibility(View.VISIBLE);
+                viewHolder.goto_pay.setVisibility(View.VISIBLE);
+            }else{
+                viewHolder.pay_status.setText("已支付");
+                viewHolder.order_cancel.setVisibility(View.GONE);
+                viewHolder.goto_pay.setVisibility(View.GONE);
+            }
+            viewHolder.order_title.setText(orderBean.getDetail_title());
+            viewHolder.course_season.setText(ConstantUtil.getSanqiItems().get(orderBean.getCourse_data().getItems()));
+            viewHolder.course_date.setText(orderBean.getCourse_date());
+            viewHolder.course_time.setText(orderBean.getChapter_times());
+            viewHolder.teacher_name.setText(orderBean.getCourse_data().getTeacher());
+            viewHolder.order_price.setText("￥" + orderBean.getAmount() / 100);
+            viewHolder.order_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
+                }
+            });
+            viewHolder.goto_pay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -44,8 +81,33 @@ public class MyOrderAdapter extends RecyclerView.Adapter<MyOrderAdapter.ViewHold
 
     static class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView order_id;
+        TextView pay_status;
+        ImageView season_img;
+        TextView order_title;
+        TextView course_season;
+        TextView course_date;
+        TextView course_time;
+        TextView teacher_name;
+        TextView pay_actual;
+        TextView order_price;
+        TextView goto_pay;
+        TextView order_cancel;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            order_id = itemView.findViewById(R.id.order_id);
+            pay_status = itemView.findViewById(R.id.pay_status);
+            season_img = itemView.findViewById(R.id.season_img);
+            order_title = itemView.findViewById(R.id.order_title);
+            course_season = itemView.findViewById(R.id.course_season);
+            course_date = itemView.findViewById(R.id.course_date);
+            course_time = itemView.findViewById(R.id.course_time);
+            teacher_name = itemView.findViewById(R.id.teacher_name);
+            pay_actual = itemView.findViewById(R.id.pay_actual);
+            order_price = itemView.findViewById(R.id.order_price);
+            goto_pay = itemView.findViewById(R.id.goto_pay);
+            order_cancel = itemView.findViewById(R.id.order_cancel);
         }
     }
 }
