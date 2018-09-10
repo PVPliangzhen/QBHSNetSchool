1a1a58d27c0c12a934813bc7ede764cd43ffb6ae
liang <810901579@qq.com>
2018-09-10 17:19:15 +0800

    上传作业界面30%，逻辑30%

--- app/src/main/java/com/qbhsnetschool/activity/SubmitHomeWorkActivity.java
+++ app/src/main/java/com/qbhsnetschool/activity/SubmitHomeWorkActivity.java
@@ -1,6 +1,7 @@
 package com.qbhsnetschool.activity;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.config.PictureConfig;
@@ -21,30 +23,29 @@ import com.qbhsnetschool.widget.vp_transforms.RotateYTransformer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SubmitHomeWorkActivity extends Activity {
+public class SubmitHomeWorkActivity extends BaseActivity {
 
-    private Button take_pic_to_upload;
-    private Button submit_pic;
     private int chooseMode = PictureMimeType.ofAll();
     private int themeId;
-    private Button take_pic_from_album;
     private List<LocalMedia> selectList = new ArrayList<>();
     private ViewPager scan_pic_pager;
     private HomeworkPicsAdapter homeworkPicsAdapter;
+    private SubmitHomeWorkActivity activity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_submit_homework);
-        take_pic_to_upload = findViewById(R.id.take_pic_to_upload);
-        submit_pic = findViewById(R.id.submit_pic);
-        take_pic_from_album = findViewById(R.id.take_pic_from_album);
-        take_pic_from_album.setOnClickListener(clickListener);
-        take_pic_to_upload.setOnClickListener(clickListener);
-        submit_pic.setOnClickListener(clickListener);
+        setBaseContentView(R.layout.activity_submit_homework, false, R.color.color_1AE200000, false);
+        activity = this;
+        ImageView camera_close = (ImageView) findViewById(R.id.camera_close);
+        camera_close.setOnClickListener(clickListener);
         themeId = R.style.picture_hlg_style;
-        scan_pic_pager = findViewById(R.id.scan_pic_pager);
-        scan_pic_pager.setPageMargin(10);
+        ImageView take_pic_img = (ImageView) findViewById(R.id.take_pic_img);
+        take_pic_img.setOnClickListener(clickListener);
+        ImageView commit_img = (ImageView) findViewById(R.id.commit_img);
+        commit_img.setOnClickListener(clickListener);
+        scan_pic_pager = (ViewPager) findViewById(R.id.scan_pic_pager);
+        scan_pic_pager.setPageMargin((int) getResources().getDimension(R.dimen.dp30));
         scan_pic_pager.setOffscreenPageLimit(5);
 
     }
@@ -53,46 +54,73 @@ public class SubmitHomeWorkActivity extends Activity {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.take_pic_from_album:
-                    // 进入相册 以下是例子：不需要的api可以不写
-                    PictureSelector.create(SubmitHomeWorkActivity.this)
-                            .openGallery(chooseMode)// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
-                            .theme(themeId)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
-                            .maxSelectNum(6)// 最大图片选择数量
-                            .minSelectNum(0)// 最小选择数量
-                            .imageSpanCount(4)// 每行显示个数
-                            .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选
-                            .previewImage(true)// 是否可预览图片
-                            .isCamera(false)// 是否显示拍照按钮
-                            .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
-                            .enableCrop(false)// 是否裁剪
-                            .compress(true)// 是否压缩
-                            .synOrAsy(true)//同步true或异步false 压缩 默认同步
-                            .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
-                            .minimumCompressSize(100)// 小于100kb的图片不压缩
-                            .selectionMedia(selectList)
-                            .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+                case R.id.camera_close:
+                    finish();
                     break;
-                case R.id.take_pic_to_upload:
-                    // 单独拍照
-                    PictureSelector.create(SubmitHomeWorkActivity.this)
-                            .openCamera(chooseMode)// 单独拍照，也可录像或也可音频 看你传入的类型是图片or视频
-                            .theme(themeId)// 主题样式设置 具体参考 values/styles
-                            .maxSelectNum(6)// 最大图片选择数量
-                            .minSelectNum(0)// 最小选择数量
-                            .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选
-                            .previewImage(true)// 是否可预览图片
-                            .isCamera(false)// 是否显示拍照按钮
-                            .enableCrop(false)// 是否裁剪
-                            .compress(true)// 是否压缩
-                            .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
-                            .minimumCompressSize(100)// 小于100kb的图片不压缩
-                            .selectionMedia(selectList)
-                            .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+                case R.id.take_pic_img:
+                    takePicImg();
                     break;
-                case R.id.submit_pic:
+                case R.id.commit_img:
+                    break;
+            }
+        }
 
-                    break;
+        private void takePicImg() {
+            try {
+                new AlertDialog.Builder(activity).setItems(
+                        new String[]{"从相册选择", "拍照上传"},
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            protected Object clone() throws CloneNotSupportedException {
+                                return super.clone();
+                            }
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case 0:// 从相册中取
+                                        // 进入相册 以下是例子：不需要的api可以不写
+                                        PictureSelector.create(SubmitHomeWorkActivity.this)
+                                                .openGallery(chooseMode)// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                                                .theme(themeId)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
+                                                .maxSelectNum(6)// 最大图片选择数量
+                                                .minSelectNum(0)// 最小选择数量
+                                                .imageSpanCount(4)// 每行显示个数
+                                                .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选
+                                                .previewImage(true)// 是否可预览图片
+                                                .isCamera(false)// 是否显示拍照按钮
+                                                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+                                                .enableCrop(false)// 是否裁剪
+                                                .compress(true)// 是否压缩
+                                                .synOrAsy(true)//同步true或异步false 压缩 默认同步
+                                                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+                                                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                                                .selectionMedia(selectList)
+                                                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+                                        break;
+                                    case 1:// 拍照
+                                        // 单独拍照
+                                        PictureSelector.create(SubmitHomeWorkActivity.this)
+                                                .openCamera(chooseMode)// 单独拍照，也可录像或也可音频 看你传入的类型是图片or视频
+                                                .theme(themeId)// 主题样式设置 具体参考 values/styles
+                                                .maxSelectNum(6)// 最大图片选择数量
+                                                .minSelectNum(0)// 最小选择数量
+                                                .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选
+                                                .previewImage(true)// 是否可预览图片
+                                                .isCamera(false)// 是否显示拍照按钮
+                                                .enableCrop(false)// 是否裁剪
+                                                .compress(true)// 是否压缩
+                                                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+                                                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                                                .selectionMedia(selectList)
+                                                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }).show();
+            }catch (Exception e){
+                e.printStackTrace();
             }
         }
     };
