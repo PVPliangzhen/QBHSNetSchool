e9e5f1230255fa1f76874e30cfb991c4dd542035
pvplz <liangzhen834973@sina.com>
2018-09-17 18:33:39 +0800

    修改刷新上课测试模块的界面的BUG

--- app/src/main/java/com/qbhsnetschool/fragment/WaitTestFragment.java
+++ app/src/main/java/com/qbhsnetschool/fragment/WaitTestFragment.java
@@ -90,7 +90,6 @@ public class WaitTestFragment extends Fragment{
             return;
         }
         LoadingDialog.loading(activity);
-        testBeans.clear();
         HttpHelper.httpGetRequest(UrlHelper.getWaitExam(), "GET", new StandardCallBack(activity) {
             @Override
             public void onSuccess(String result) {
@@ -151,6 +150,7 @@ public class WaitTestFragment extends Fragment{
                     Gson gson = new Gson();
                     testBeans = gson.fromJson(msg.toString(), new TypeToken<List<TestBean>>(){}.getType());
                 }else{
+                    testBeans.clear();
                     String msg = jsonObject.optString("msg");
                     Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show();
                 }
