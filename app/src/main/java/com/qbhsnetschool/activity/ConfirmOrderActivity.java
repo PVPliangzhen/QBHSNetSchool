23247ee23fa82938d1c47e187c5f2e4bbf84775d
liang <810901579@qq.com>
2018-09-17 23:11:10 +0800

    Revert "完善gradle"
    This reverts commit ffd728c38f126178c1c648419d1fc671c2af9d8e.

--- app/src/main/java/com/qbhsnetschool/activity/ConfirmOrderActivity.java
+++ app/src/main/java/com/qbhsnetschool/activity/ConfirmOrderActivity.java
@@ -436,6 +436,10 @@ public class ConfirmOrderActivity extends BaseActivity{
                     Toast.makeText(activity, "支付失败", Toast.LENGTH_SHORT).show();
                     break;
                 case "success":
+                    //Toast.makeText(activity, "支付成功", Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent();
+//                    intent.setClass(activity, PurchaseSuccessActivity.class);
+//                    startActivity(intent);
                     if (!UIUtils.isNetworkAvailable(activity)){
                         Toast.makeText(activity, R.string.no_network, Toast.LENGTH_SHORT).show();
                         return;
